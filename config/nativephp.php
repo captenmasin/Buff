5e3ddc577b4e729711c64ba53e65353f8d38f392
Mason Day <daymason@hotmail.co.uk>
2026-05-27 09:57:25 +0100

    Orientation

--- config/nativephp.php
+++ config/nativephp.php
@@ -293,8 +293,8 @@ return [
         'android' => [
             'portrait' => true,
             'upside_down' => false,
-            'landscape_left' => false,
-            'landscape_right' => false,
+            'landscape_left' => true,
+            'landscape_right' => true,
         ],
     ],
 ];
